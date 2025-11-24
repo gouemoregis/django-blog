1a3d293d4d0cd267ad475a9de6c55b1b5dfde440
gouemoregis <gouemoregis@gmail.com>
2025-11-24 23:27:52 +0100

    handling search in pages

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
 
-from blogs.models import Blog, Category
+from assignments.models import About
+from blogs.models import Blog
 
 def home(request):
     featured_posts = Blog.objects.filter(is_featured=True, status="published")
     posts = Blog.objects.filter(is_featured=False, status="published")
+    
+    # Fetch about us information
+    try:
+        about = About.objects.get()
+    except:
+        about = None
+        
     context = {
         "featured_posts": featured_posts,
         "posts": posts,
+        "about": about,
     }
     return render(request, 'home.html', context)
